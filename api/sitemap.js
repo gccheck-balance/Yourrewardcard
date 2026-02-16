6358e99778e88c1da3029d5db33d0940e4a2d21c
khachatryanmimi-design <khachatryanmimi@gmail.com>
2026-02-16 19:53:55 +0400

    SEO: optimize titles, descriptions, keywords, add structured data and canonical URLs for gift card balance search ranking

--- api/sitemap.js
+++ api/sitemap.js
@@ -2,25 +2,25 @@ const SITEMAP_XML = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://www.yourrewarrdcard.com/</loc>
-    <lastmod>2026-02-14</lastmod>
+    <lastmod>2026-02-16</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
     <loc>https://www.yourrewarrdcard.com/faq</loc>
-    <lastmod>2026-02-14</lastmod>
+    <lastmod>2026-02-16</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>https://www.yourrewarrdcard.com/contact</loc>
-    <lastmod>2026-02-14</lastmod>
+    <lastmod>2026-02-16</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://www.yourrewarrdcard.com/about</loc>
-    <lastmod>2026-02-14</lastmod>
+    <loc>https://www.yourrewarrdcard.com/learnmore</loc>
+    <lastmod>2026-02-16</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.8</priority>
   </url>
